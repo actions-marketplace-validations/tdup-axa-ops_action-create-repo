c1c3b86e0f9c29c627ebeba4ff42fddda4dcce2a
ndupoiron <123163496+ndupoiron@users.noreply.github.com>
2023-05-11 07:45:10 +0000

    create repo action

---
+++ index.js
@@ -0,0 +1,33 @@
+const core = require('@actions/core');
+const { Octokit } = require("@octokit/rest");
+
+// Get Input Parameters
+const repoOwner = core.getInput('repo_owner');
+const repoName = core.getInput('repo_name');
+const repoVisibility = core.getInput('repo_visibility');
+const token = core.getInput('token')
+
+const octokit = new Octokit({
+    auth: token
+  });
+  
+async function createRepository(repoOwner, repoName, repoVisibility) {
+
+    octokit.rest.repos.createInOrg({
+        org: repoOwner,
+        name: repoName,
+        visibility: repoVisibility
+    }).then(({ data }) => {
+        core.setOutput("repo_full_name", data.full_name);
+        core.setOutput("repo_url", data.html_url);
+        core.setOutput("repo_id", data.id);
+        core.setOutput("status", "success");
+    }
+    ).catch((error) => {
+        core.setFailed(error.response.data.message);
+        core.setOutput("status", "failure");
+    }
+    );
+}
+
+createRepository(repoOwner, repoName, repoVisibility);
